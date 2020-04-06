d9a235d417a4e24781a3df067ab4c8e75c692c0a
LarsBreuren <larsbreuren@gmail.com>
2020-04-06 16:43:30 +0200

    removed old js

--- public/js/featureDetection.js
+++
@@ -1,7 +0,0 @@
-const conditional = document.querySelector('.conditional');
-const testElem = document.createElement('div');
-if (testElem.style.flex !== undefined && testElem.style.flexFlow !== undefined) {
-  conditional.setAttribute('href', 'flex-layout.css');
-} else {
-  conditional.setAttribute('href', 'float-layout.css');
-}
--- public/js/print.js
+++
@@ -1,6 +0,0 @@
-let printsheet = document.getElementById("print")
-    printsheet.addEventListener("click", printPage);
-
-    function printPage(){
-        window.print();
-    }
