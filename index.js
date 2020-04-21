a1e01fe268290df82653cd1233e5a082b58b8aa8
LarsBreuren <larsbreuren@gmail.com>
2020-04-21 13:56:47 +0200

    New chat app. Added scores.

---
+++ index.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const fetch = require('node-fetch')
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+app.get('/chat', function(req, res) {
+    res.render('chat.ejs');
+});
+
+app.get('/movies', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.MOVIEDB_TOKEN}`)
+      .then(async response => {
+        const movieData = await response.json()
+        console.log(movieData);
+        let randomItem = movieData.results[Math.random() * movieData.results.length | 0];
+        res.render('movies', {
+          title: 'Movies',
+          randomMovie: randomItem,
+          movieData,
+        });
+      })
+  })
+
+  let movieTitle = '';
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.sockets.on('connection', function(socket) {
+
+    randomMovie();
+
+    socket.on('username', function(username, score) {
+        socket.username = username;
+        socket.score = score;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' ' + '[' + socket.score + ']' + ' join the chat..</i>');
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    })
+
+    socket.on('chat_message', function(message, score) {
+        if (message == movieTitle) {
+            socket.score++
+            io.emit('chat_message', '<strong>' + socket.username + '[' + socket.score + ']' + '</strong>: ' + message);
+            io.emit('chat_message', ('Server', 'Die is goed! ' + socket.username + ' +1'));
+            randomMovie();
+        }
+        else{
+            io.emit('chat_message', '<strong>' + socket.username + '[' + socket.score + ']' + '</strong>: ' + message);
+         }
+    });
+
+});
+
+
+function randomMovie(){
+
+    fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.MOVIEDB_TOKEN}`)
+    .then(async response => {
+      const movieData = await response.json()
+      let randomItem = movieData.results[Math.random() * movieData.results.length | 0];
+
+      // Welcome current user
+      io.emit('chat_message', ('server', '<div class="server">' + 'Welcome to real time chat!' + "<br><br>" + '<strong>' + 'What movie is this?' + '</strong>' + "<br>" + randomItem.overview + '</div>'));
+      movieTitle = randomItem.original_title;
+      console.log('Antwoord = ' + movieTitle);
+    })
+
+}
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- public/js/main.js
+++ public/js/main.js
@@ -1,113 +1,24 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
+var socket = io.connect('http://localhost:3000');
 
-// Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true
+// submit text message without reload/refresh the page
+$('form').submit(function(e){
+    e.preventDefault(); // prevents page reloading
+    socket.emit('chat_message', $('#txt').val());
+    $('#txt').val('');
+    return false;
 });
 
-const socket = io();
-
-// Join chatroom
-socket.emit('joinRoom', { username, room });
-
-// Get room and users
-socket.on('roomUsers', ({ room, users }) => {
-  outputRoomName(room);
-  outputUsers(users);
+// append the chat text message
+socket.on('chat_message', function(msg){
+    $('#messages').append($('<li>').html(msg));
 });
 
-// Message from server
-socket.on('message', message => {
-  console.log(message);
-  outputMessage(message);
-  // Scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
+// ask username
+var username = prompt('Please tell me your name');
+var score = 0;
+socket.emit('username', username, score);
+
+// append text if someone is online
+socket.on('is_online', function(username) {
+    $('#messages').append($('<li>').html(username));
 });
-
-// Message submit
-chatForm.addEventListener('submit', e => {
-  e.preventDefault();
-
-  // Get message text
-  const msg = e.target.elements.msg.value;
-
-  // Emit message to server
-  socket.emit('chatMessage', msg);
-
-  // Clear input
-  e.target.elements.msg.value = '';
-  e.target.elements.msg.focus();
-});
-
-// Output message to DOM
-function outputMessage(message, room) {
-  if(message.text == '/help'){
-    commandResponse = "Need some help mate?"
-    commands(commandResponse);
-   } 
-   else if(message.text == '/marco'){
-    commandResponse = "Polo"
-    commands(commandResponse);
-   }  
-   else if(message.text.includes("mobile") &&  roomName.innerText == "Games" ){
-    // window.location.href = '/'; optional kick
-    commandResponse = "Someone spoke of mobile games, this chat is now destroyed x.x";
-    commands(commandResponse);
-    socket.emit('forceDisconnect');
-     
-   }
-   else if(message.text.includes("hamilton") &&  roomName.innerText == "Formula 1" ){
-    // window.location.href = '/'; optional kick
-    commandResponse = "Someone spoke of Hamilton, this chat is now finished";
-    commands(commandResponse);
-    socket.emit('forceDisconnect');
-   }
-   else if(message.text.includes("prius") &&  roomName.innerText == "Cars" ){
-    // window.location.href = '/'; optional kick
-    commandResponse = "Someone spoke of the prius, this chat is now done";
-    commands(commandResponse);
-    socket.emit('forceDisconnect');
-   }
-   else if(message.text.includes("innerHTML") &&  roomName.innerText == "JavaScript" ){
-    // window.location.href = '/'; optional kick
-    commandResponse = "No innerHTMl allowed! throw new Error();";
-    commands(commandResponse);
-    socket.emit('forceDisconnect');
-   }
-   else {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
-  <p class="text">
-    ${message.text}
-  </p>`;
-  document.querySelector('.chat-messages').appendChild(div);
-   }
-}
-
-function commands(commandResponse){
-const div = document.createElement('div');
-  div.classList.add('message');
-  div.innerHTML = `<p class="meta">${'Server'}</p>
-  <p class="text">
-    ${commandResponse}
-  </p>`;
-  document.querySelector('.chat-messages').appendChild(div);
-}
-
-
-
-// Add room name to DOM
-function outputRoomName(room) {
-  roomName.innerText = room;
-}
-
-// Add users to DOM
-function outputUsers(users) {
-  userList.innerHTML = `
-    ${users.map(user => `<li>${user.username}</li>`).join('')}
-  `;
-}
