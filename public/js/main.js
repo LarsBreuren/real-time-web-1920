ae7d9400050dda4982422b036f9ac5878be705a3
LarsBreuren <larsbreuren@gmail.com>
2020-04-21 14:39:50 +0200

    Bye jquery, hello vanilla JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,27 @@
 var socket = io.connect('http://localhost:3000');
 
 // submit text message without reload/refresh the page
-$('form').submit(function(e){
-    e.preventDefault(); // prevents page reloading
-    socket.emit('chat_message', $('#txt').val());
-    $('#txt').val('');
+document.getElementById("chatForm").onsubmit = function(event){
+    event.preventDefault();
+    socket.emit('chat_message', document.getElementById("txt").value);
+    document.getElementById("txt").value = '';
     return false;
-});
+}
 
-// append the chat text message
 socket.on('chat_message', function(msg){
-    $('#messages').append($('<li>').html(msg));
+    let newLI = document.createElement("LI");
+    newLI.innerHTML = msg;
+
+    document.getElementById("messages").appendChild(newLI);
 });
 
-// ask username
 var username = prompt('Please tell me your name');
 var score = 0;
 socket.emit('username', username, score);
 
-// append text if someone is online
 socket.on('is_online', function(username) {
-    $('#messages').append($('<li>').html(username));
+    let newLI = document.createElement("LI");
+    newLI.innerHTML = username;
+
+    document.getElementById("messages").appendChild(newLI);
 });
