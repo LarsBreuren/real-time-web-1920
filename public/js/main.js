2d254d8423244bc7db69a014affcb9b4dfac86a4
LarsBreuren <larsbreuren@gmail.com>
2020-04-21 15:02:11 +0200

    trying to fix heroku

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io();
+
 
 // submit text message without reload/refresh the page
 document.getElementById("chatForm").onsubmit = function(event){
